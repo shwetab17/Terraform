228f4b4f823a7ebdc72ae8c50bb662b201c186e9
shwetab17 <70053804+shwetab17@users.noreply.github.com>
2021-05-18 10:57:20 +0530

    Update init_webserver.sh
    user data script

--- init_webserver.sh
+++ init_webserver.sh
@@ -11,6 +11,7 @@
   systemctl start nginx
   systemctl enable nginx
   
+# Print the hostname which includes instance details on nginx homepage  
   sudo echo Hello from `hostname -f` > /usr/share/nginx/html/index.html
 
   
